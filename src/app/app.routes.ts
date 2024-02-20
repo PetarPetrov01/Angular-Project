71e51f8a0ccbb76ddf0e63a4f2613aeb5bde44f7
PetarPetrov01 <petyr_p@mail.bg>
2024-02-20 19:14:49 +0200

    Routes and images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
+import { ProductsComponent } from './main/products/products.component';
 
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
   },
 ];
