2adbd3b73bda80524016ab065d0f3e56e3febef0
PetarPetrov01 <petyr_p@mail.bg>
2024-02-25 10:53:02 +0200

    Error validation styling

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,13 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Form, FormControl, FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, NgIf],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-
+  handleRegister(form: NgForm) {
+    console.log(form.value);
+  }
 }
