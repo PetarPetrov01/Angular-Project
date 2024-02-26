221609b7cb77ec65b97dc6ce10e25fb224cc912b
PetarPetrov01 <petyr_p@mail.bg>
2024-02-26 20:41:16 +0200

    Email Validator directive

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,19 +1,32 @@
-import { NgIf } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { JsonPipe, NgIf } from '@angular/common';
+import { Component, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { MatchPasswordsDirective } from '../../validators/match-passwords.directive';
+import { EmailValidateDirective } from '../../validators/email-validator.directive';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, FormsModule, NgIf, MatchPasswordsDirective],
+  imports: [
+    RouterLink,
+    FormsModule,
+    NgIf,
+    MatchPasswordsDirective,
+    EmailValidateDirective,
+    JsonPipe
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {
-  @ViewChild('passwords',{static:false}) passwords! : NgModel;
+export class RegisterComponent implements OnChanges{
+  @ViewChild('passwords', { static: false }) passwords!: NgModel;
+  @ViewChild('email', { static: false }) email!: NgModel;
 
+  ngOnChanges(changes: SimpleChanges): void {
+
+    console.log(this.email.errors)
+  }
   handleRegister(form: NgForm) {
     // console.log(form.getControl(this.passwords));
   }
---
+++ src/app/validators/email-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { EmailValidateDirective } from './email-validator.directive';
+
+describe('EmailValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new EmailValidateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
