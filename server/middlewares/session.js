fa93b245c1c3cca1c09f963e49c7aaefbd9d3894
PetarPetrov01 <petyr_p@mail.bg>
2024-02-29 11:42:47 +0200

    Session middleware

--- server/middlewares/session.js
+++ server/middlewares/session.js
@@ -1,17 +1,18 @@
-const { authCookie } = require("../config/cookie.js");
+const { authCookieName } = require("../config/cookie.js");
+const authService = require("../services/authService.js");
 
 module.exports = () => (req, res, next) => {
-  const token = req.cookies[authCookie];
+  const token = req.cookies[authCookieName];
 
   if (token) {
     try {
-        // const payload = verify token;
-        // req.user = payload;
-        // req.token = token;
+      const payload = authService.verifyToken(token);
+      req.user = payload;
+      // req.token = token;
     } catch (error) {
-        res.status(401).json({message: 'Invalid auth token'})
+      res.status(401).json({ message: "Invalid auth token" })
     }
-  } 
+  }
 
   next();
 };
