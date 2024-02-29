9b9947812a69203d3b08b95476044649930114a5
PetarPetrov01 <petyr_p@mail.bg>
2024-02-29 18:56:39 +0200

    Auth service implementation

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,11 +1,43 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+import { User } from '../types/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private user$$ = new BehaviorSubject<User | undefined>(undefined);
+  public user$ = this.user$$.asObservable();
 
-  constructor(private http: HttpClient) { }
+  user: User | undefined;
 
+  subscription: Subscription;
+
+  constructor(private http: HttpClient) {
+    this.subscription = this.user$$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<User>('/api/auth/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  register(email: string, username: string, password: string) {
+    return this.http
+      .post<User>('/api/auth/register', {
+        email,
+        username,
+        password,
+      })
+      .subscribe({
+        next: (user) => this.user$$.next(user),
+        error: (err)=>{
+          console.log(err)
+        }
+      });
+  }
 }
