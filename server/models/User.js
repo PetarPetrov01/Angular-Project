1afc83c885c539128183100de625899ff66bbb05
PetarPetrov01 <petyr_p@mail.bg>
2024-02-29 10:14:56 +0200

    Register service func

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, },
 });
 
 userSchema.index({username},{
--- server/services/authService.js
+++ server/services/authService.js
@@ -1,38 +1,67 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const secret = process.env.JWT_SECRET || ((Math.random()*10000).toFixed(0).toString(16));
+const secret =
+  process.env.JWT_SECRET || (Math.random() * 10000).toFixed(0).toString(16);
 
 async function login(email, password) {
-    const existingUser = User.find({email}).collation({locale: 'en',strength: 2})
-    if (!existingUser){
-        throw new Error('Invalid email or password')
-    }
+  const existingUser = User.find({ email }).collation({
+    locale: "en",
+    strength: 2,
+  });
+  if (!existingUser) {
+    throw new Error("Invalid email or password");
+  }
 
-    const matchPass = await bcrypt.compare(password,existingUser.hashedPassword)
-    if(matchPass){
-        throw new Error('Invalid email or password')
-    }
+  const matchPass = await bcrypt.compare(password, existingUser.hashedPassword);
+  if (matchPass) {
+    throw new Error("Invalid email or password");
+  }
 
-    return createToken(existingUser)
+  return createToken(existingUser);
 }
+
+async function register(username, email, password) {
+  const existingUser = User.find({ email }).collation({
+    locale: "en",
+    strength: 2,
+  });
+
+  if (existingUser) {
+    throw new Error("This email is already taken");
+  }
+
+  const user = await User.create({
+    email,
+    username,
+    hashedPassword: await bcrypt.hash(password, 10),
+  });
+
+  return createToken(user);
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, secret);
+}
+
 function createToken(user) {
+  const payload = {
+    _id: user._id,
+    email: user.email,
+  };
 
-    const payload ={
-        _id: user._id,
-        email: user.email
-    }
+  const token = jwt.sign(payload);
 
-    const token = jwt.sign(payload)
-    
-    return {
-        _id: user._id,
-        email: user.email,
-        accessToken: jwt.sign(payload, secret)
-    }
+  return {
+    _id: user._id,
+    email: user.email,
+    authToken: jwt.sign(token, secret, { expiresIn: "2d" }),
+  };
 }
 
 module.exports = {
-    login,
-}
+  login,
+  register,
+  verifyToken,
+};
