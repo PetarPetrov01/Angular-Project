ed192db90541851ff1b5a9d1a7a8c0db8f5900d1
PetarPetrov01 <petyr_p@mail.bg>
2024-03-01 10:50:01 +0200

    Session component

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+
+export const cookieName = 'auth-cookie';
 
 @Component({
   selector: 'app-auth',
@@ -7,13 +11,26 @@ import { CookieService } from 'ngx-cookie-service';
   imports: [],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
-  providers: [CookieService]
+  providers: [CookieService],
 })
-export class AuthComponent implements OnInit{
-  
-  constructor (private cookieService: CookieService){}
+export class AuthComponent implements OnInit {
+  constructor(
+    private cookieService: CookieService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    console.log(this.cookieService.get('auth-cookie'))
-  }
+    const user = this.authService.getUser();
+    const authCookie = this.cookieService.get(cookieName);
 
+    if (user && authCookie) {
+      //Both credentials intact
+      console.log('credentials intact')
+    } else if(authCookie){
+      //Check if token is valid
+      console.log('missing user')
+    } else {
+      console.log('Token is missing')
+    }
+  }
 }
