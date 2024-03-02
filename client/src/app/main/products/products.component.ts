449c94bcf691e4759da6e9c052582b1768febf02
PetarPetrov01 <petyr_p@mail.bg>
2024-03-02 17:03:30 +0200

    Mock products

--- client/src/app/main/products/products.component.ts
+++ client/src/app/main/products/products.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent {
-  
+  mockProds = [
+    { img: 'assets/images/chair-modern.png', price: 199 },
+    { img: 'assets/images/stool.png', price: 199 },
+    { img: 'assets/images/lamp.png', price: 199 },
+    { img: 'assets/images/chair-modern.png', price: 199 },
+  ];
 }
