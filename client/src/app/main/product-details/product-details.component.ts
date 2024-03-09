9e975f2290ae50785684a76d82845d78fb8a7f6b
PetarPetrov01 <petyr_p@mail.bg>
2024-03-09 17:50:17 +0200

    Styling

--- client/src/app/main/product-details/product-details.component.ts
+++ client/src/app/main/product-details/product-details.component.ts
@@ -20,16 +20,24 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
   constructor(
     private activated: ActivatedRoute,
     private apiService: ApiService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.activated.params.subscribe((params) => {
       this.productId = params['id'];
-      this.subscription = this.apiService.getProduct(this.productId).subscribe(prod=>{
-        this.product = prod;
-      })
+      this.subscription = this.apiService
+        .getProduct(this.productId)
+        .subscribe((prod) => {
+          this.product = prod;
+        });
     });
+  }
+
+  monthlyPrice(price: number | undefined): string {
+    const promotion = 0.05;
+    const monthly = Number(price) / 12;
+    console.log(monthly);
+    return (monthly * (1 - promotion)).toFixed(2);
   }
 
   ngOnDestroy(): void {
