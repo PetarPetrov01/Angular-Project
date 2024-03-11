bacfb2131bfc5dc1dafd8ef7676aeadeeec6d54c
PetarPetrov01 <petyr_p@mail.bg>
2024-03-11 23:38:55 +0200

    Modify User model (cart added)

--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,28 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true },
-  hashedPassword: { type: String, required: true }
+  cart: {
+    type: [
+      {
+        productId: { type: Types.ObjectId, ref: "Product" },
+        quantity: { type: Number },
+      },
+    ],
+  },
+  hashedPassword: { type: String, required: true },
 });
 
-userSchema.index({email:1}, {
+userSchema.index(
+  { email: 1 },
+  {
     collation: {
-        locale: 'en',
-        strength: 2
-    }
-})
+      locale: "en",
+      strength: 2,
+    },
+  }
+);
 
 const User = model("User", userSchema);
 
