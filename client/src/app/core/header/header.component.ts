fc9cb4f23976efca564a94079f0bfb956a15abe8
PetarPetrov01 <petyr_p@mail.bg>
2024-03-16 14:45:57 +0200

    Implement products count badge in the cart header

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -7,21 +7,32 @@ import {
   RouterLinkActive,
 } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
-
 import { MatMenuModule } from '@angular/material/menu';
+import { MatBadgeModule } from '@angular/material/badge';
+import { CartState } from '../../types/State';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgIf, MatMenuModule],
+  imports: [RouterLink, RouterLinkActive, NgIf, MatMenuModule, MatBadgeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  cartQuantity: number = 0;
+
   constructor(
     private router: Router,
     private authService: AuthService,
-  ) {}
+    private store: Store<CartState>
+  ) {
+    this.store
+      .select('cart')
+      .subscribe((prods) =>
+      this.cartQuantity = prods.reduce((acc, prod) => (acc += prod.quantity), 0)
+      );
+  }
 
   get isLogged() {
     return this.authService.isLogged;
@@ -33,7 +44,7 @@ export class HeaderComponent {
   }
 
   //To ensure visual indication on the parent anchor(dropdown)
-  isProfileActive(){
-   return this.router.url.includes('/profile')
+  isProfileActive() {
+    return this.router.url.includes('/profile');
   }
 }
