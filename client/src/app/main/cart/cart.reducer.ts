32b02899d320febffe353584a1d6a470ec003153
PetarPetrov01 <petyr_p@mail.bg>
2024-03-16 11:07:42 +0200

    Reducer logic

--- client/src/app/main/cart/cart.reducer.ts
+++ client/src/app/main/cart/cart.reducer.ts
@@ -26,4 +26,26 @@ const initial: StateProduct[] | [] = [
 
 export const cartReducer = createReducer(
   initial,
+  on(actions.addItem, (state, { product, qty }) => {
+    return state.some((prod) => prod._id == product._id)
+      ? state.map((prod) => {
+          return prod._id == product._id
+            ? {
+                ...prod,
+                quantity: prod.quantity + qty,
+              }
+            : prod;
+        })
+      : [...state, { ...product, quantity: qty }];
+  }),
+  on(actions.decreaseQuantity, (state, { productId }) => {
+    return state.map((prod) => {
+      return prod._id == productId
+        ? { ...prod, quantity: prod.quantity - 1 }
+        : prod;
+    });
+  }),
+  on(actions.removeItem, (state, { productId }) => {
+    return state.filter((prod) => prod._id != productId);
+  })
 );
