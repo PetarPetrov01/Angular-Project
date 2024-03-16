7dbcb2579000f675b5fa6e87d25040a11d6d415b
PetarPetrov01 <petyr_p@mail.bg>
2024-03-16 16:05:28 +0200

    Cart component logic

--- client/src/app/main/cart/cart.component.ts
+++ client/src/app/main/cart/cart.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ApiService } from '../api.service';
 import { State, Store } from '@ngrx/store';
 import { CartState, StateProduct } from '../../types/State';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
-import * as CartActions from '../cart/cart.actions'
+import * as CartActions from '../cart/cart.actions';
+import { PopulatedProduct } from '../../types/Product';
+import { MatDialog } from '@angular/material/dialog';
+import { RemoveDialogComponent } from './remove-dialog/remove-dialog.component';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-cart',
@@ -14,29 +18,67 @@ import * as CartActions from '../cart/cart.actions'
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
   products$ = new Observable<StateProduct[]>();
+  products: StateProduct[] | null = null;
 
-  constructor(private apiService: ApiService, private store: Store<CartState>) {
+  prodsSubscription: Subscription | null = null;
+
+  constructor(
+    private store: Store<CartState>,
+    private matDialog: MatDialog,
+    private authService: AuthService
+  ) {
     this.products$ = this.store.select('cart');
-    this.products$.subscribe(prods=> console.log(prods));
+    this.prodsSubscription = this.products$.subscribe((prods) => {
+      this.products = prods;
+    });
   }
 
-  handleIncreaseQuantity(currentProduct: StateProduct){
-    this.store.dispatch(CartActions.addItem({product: currentProduct,qty: 1}))
+  handleIncreaseQuantity(currentProduct: StateProduct) {
+    this.store.dispatch(
+      CartActions.addItem({ product: currentProduct, qty: 1 })
+    );
   }
 
-  handleDecreaseQuantity(currentProduct: StateProduct){
-    if (currentProduct.quantity <=1){
-      this.store.dispatch(CartActions.removeItem({productId: currentProduct._id}))
+  handleDecreaseQuantity(currentProduct: StateProduct) {
+    if (currentProduct.quantity <= 1) {
+      this.matDialog.open(RemoveDialogComponent, {
+        width: '300px',
+        enterAnimationDuration: '300ms',
+        exitAnimationDuration: '200ms',
+        data: {
+          productName: currentProduct?.name,
+          _id: currentProduct?._id,
+        },
+      });
     } else {
-      this.store.dispatch(CartActions.decreaseQuantity({productId : currentProduct._id}))
+      this.store.dispatch(
+        CartActions.decreaseQuantity({ productId: currentProduct._id })
+      );
     }
   }
 
+  get totalCount() {
+    return this.products?.reduce((acc, prod) => acc + prod.quantity, 0);
+  }
 
-  ngOnInit(): void {
-    // this.apiService.getProductsInCart();
-    // this.products = prods;
+  get totalPrice() {
+    return this.products?.reduce(
+      (acc, prod) => acc + prod.quantity * prod.price,
+      0
+    );
+  }
+
+  isInWishList(product: StateProduct) {
+    return !!this.authService.user?.wishlist.some((prod) => prod == product._id)
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    if (this.prodsSubscription) {
+      this.prodsSubscription.unsubscribe();
+    }
   }
 }
