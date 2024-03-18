6eefaccdde8dca407dc1c066e336bb9f52c65fa9
PetarPetrov01 <petyr_p@mail.bg>
2024-03-18 19:13:06 +0200

    Home page modification

--- client/src/app/main/home/home.component.ts
+++ client/src/app/main/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../shared/api.service';
+import { Observable } from 'rxjs';
+import { APIProduct } from '../../types/Product';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent{
+products$ : Observable<APIProduct[]>; 
 
+constructor (private apiService: ApiService){
+  this.products$ = this.apiService.getProducts({limit: 3, sort: 'createdAt asc'});
+
+}
 }
