58ec13fd7db0bd1108209895487abced7e697c61
PetarPetrov01 <petyr_p@mail.bg>
2024-03-28 20:14:31 +0200

    Home page loader

--- client/src/app/main/home/home.component.ts
+++ client/src/app/main/home/home.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/api.service';
-import { Observable } from 'rxjs';
+import {  Subscription } from 'rxjs';
 import { APIProduct } from '../../types/Product';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { LoaderCardComponent } from '../../shared/loader-card/loader-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, LoaderCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent{
-products$ : Observable<APIProduct[]>; 
+export class HomeComponent implements OnInit, OnDestroy{
+products: APIProduct[] | [] = [];
+isLoading: boolean = false;
+
+subscription: Subscription | null = null;
 
 constructor (private apiService: ApiService){
-  this.products$ = this.apiService.getProducts({limit: 3, sort: 'createdAt:asc'});
-
 }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    
+    this.subscription = this.apiService.getProducts({limit: 3, sort: 'createdAt:asc'}).subscribe(products=>{
+      
+      setTimeout(()=>{
+        this.products = products;
+        this.isLoading = false;
+      },2000)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe()
+  }
 }
