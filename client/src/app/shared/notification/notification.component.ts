d2462dad525a9ce94f317712136b33b6e100d400
PetarPetrov01 <petyr_p@mail.bg>
2024-04-16 16:31:10 +0300

    Notification service

--- client/src/app/shared/notification/notification.component.ts
+++ client/src/app/shared/notification/notification.component.ts
@@ -1,13 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NotificationService } from './notification.service';
 
 @Component({
   selector: 'app-notification',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './notification.component.html',
-  styleUrl: './notification.component.css'
+  styleUrl: './notification.component.css',
 })
-export class NotificationComponent {
+export class NotificationComponent implements OnInit {
+  notification: null | string = null;
+
+  notificationService = inject(NotificationService);
+
+  ngOnInit(): void {
+    this.notificationService.notificaiton$.subscribe((notification) => {
+      this.notification = notification;
+    });
+  }
+
+  clearNotification(){
+    this.notificationService.clearNotification();
+  }
 
 }
--- client/src/app/shared/notification/notification.service.ts
+++ client/src/app/shared/notification/notification.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
+  notification$$ = new BehaviorSubject<null | string>(null);
+  notificaiton$ = this.notification$$.asObservable();
+ 
+  constructor() {}
 
-  constructor() { }
+  setNotification(notification: string) {
+    this.notification$$.next(notification);
+
+    setTimeout(() => {
+      this.clearNotification()
+    }, 4000);
+  }
+
+  clearNotification() {
+    this.notification$$.next(null);
+  }
+
 }
