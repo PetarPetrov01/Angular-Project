75f432d9bfa88f46f32011ec34847b94d03a2f89
PetarPetrov01 <petyr_p@mail.bg>
2024-04-22 22:46:18 +0300

    Clear diaolog component test setup

--- client/src/app/auth/cart/clear-dialog/clear-dialog.component.spec.ts
+++ client/src/app/auth/cart/clear-dialog/clear-dialog.component.spec.ts
@@ -1,17 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClearDiaologComponent } from './clear-dialog.component';
+import { Store } from '@ngrx/store';
 
 describe('ClearDiaologComponent', () => {
   let component: ClearDiaologComponent;
   let fixture: ComponentFixture<ClearDiaologComponent>;
+  let storeMock: jasmine.SpyObj<Store>;
 
   beforeEach(async () => {
+    storeMock = jasmine.createSpyObj('Store', ['dispatch']);
+
     await TestBed.configureTestingModule({
-      imports: [ClearDiaologComponent]
-    })
-    .compileComponents();
-    
+      imports: [ClearDiaologComponent],
+      providers: [
+        {
+          provide: Store,
+          useValue: storeMock,
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ClearDiaologComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
