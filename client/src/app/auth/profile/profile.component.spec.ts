20a514b18e9a593cdc92017b98c6d1f518287e4d
PetarPetrov01 <petyr_p@mail.bg>
2024-04-23 18:06:54 +0300

    Setup profile component testing

--- client/src/app/auth/profile/profile.component.spec.ts
+++ client/src/app/auth/profile/profile.component.spec.ts
@@ -1,23 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
+import { AuthService } from '../../shared/auth.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CommonModule } from '@angular/common';
+import { EMPTY, of } from 'rxjs';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
+  let matDialogMock: jasmine.SpyObj<MatDialog>;
+  let routerMock: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', [
+      'user$',
+      'getOwnProducts',
+      'clearUserSession',
+    ]);
+    matDialogMock = jasmine.createSpyObj('MatDialog', ['open']);
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
-      imports: [ProfileComponent]
-    })
-    .compileComponents();
-    
+      imports: [ProfileComponent, RouterTestingModule, CommonModule],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: MatDialog, useValue: matDialogMock },
+      ],
+    }).compileComponents();
+
+    authServiceMock.user$ = of({
+      _id: '',
+      username: '',
+      email: '',
+      password: '',
+      wishlist: [],
+    });
+    authServiceMock.getOwnProducts.and.returnValue(EMPTY);
+
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(true).toBeTruthy();
   });
 });
