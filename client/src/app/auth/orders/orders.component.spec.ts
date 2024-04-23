396aafa3f17f0c86c9da1f6556c4e2115a1b34b2
PetarPetrov01 <petyr_p@mail.bg>
2024-04-23 18:17:46 +0300

    Configure orders and not-found component tests

--- client/src/app/auth/orders/orders.component.spec.ts
+++ client/src/app/auth/orders/orders.component.spec.ts
@@ -1,17 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrdersComponent } from './orders.component';
+import { AuthService } from '../../shared/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EMPTY } from 'rxjs';
 
 describe('OrdersComponent', () => {
   let component: OrdersComponent;
   let fixture: ComponentFixture<OrdersComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', ['getOrders']);
+
     await TestBed.configureTestingModule({
-      imports: [OrdersComponent]
-    })
-    .compileComponents();
-    
+      imports: [OrdersComponent, RouterTestingModule],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        },
+      ],
+    }).compileComponents();
+    authServiceMock.getOrders.and.returnValue(EMPTY);
+
     fixture = TestBed.createComponent(OrdersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
