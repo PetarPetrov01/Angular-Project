03e3a74f70e41a72bab46beb1264082ffa99b71a
PetarPetrov01 <petyr_p@mail.bg>
2024-04-23 18:34:11 +0300

    setup auth and register component testing

--- client/src/app/auth/auth.component.spec.ts
+++ client/src/app/auth/auth.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
+import { AuthService } from '../shared/auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
+  let cookieServiceMock: jasmine.SpyObj<CookieService>;
 
   beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', [
+      'getUserStorage',
+      'setUserSubject',
+      'getProfile',
+      'clearUserSession',
+    ]);
+    cookieServiceMock = jasmine.createSpyObj('CookieService', ['get']);
+
     await TestBed.configureTestingModule({
-      imports: [AuthComponent]
-    })
-    .compileComponents();
-    
+      imports: [AuthComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: CookieService, useValue: cookieServiceMock },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- client/src/app/auth/register/register.component.spec.ts
+++ client/src/app/auth/register/register.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { AuthService } from '../../shared/auth.service';
+import { keyframes } from '@angular/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', ['register']);
+
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
-    })
-    .compileComponents();
-    
+      imports: [RegisterComponent, RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
