962d3d31a28be1df650395cf3371b4fc264b7387
PetarPetrov01 <petyr_p@mail.bg>
2024-04-24 22:03:26 +0300

    Add product and product components test setup

--- client/src/app/main/add-product/add-product.component.spec.ts
+++ client/src/app/main/add-product/add-product.component.spec.ts
@@ -1,17 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddProductComponent } from './add-product.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiService } from '../../shared/api.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddProductComponent', () => {
   let component: AddProductComponent;
   let fixture: ComponentFixture<AddProductComponent>;
+  let apiServiceMock: jasmine.SpyObj<ApiService>;
 
   beforeEach(async () => {
+    apiServiceMock = jasmine.createSpyObj('ApiService', ['getProduct']);
+
     await TestBed.configureTestingModule({
-      imports: [AddProductComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        AddProductComponent,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [{ provide: ApiService, useValue: apiServiceMock }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AddProductComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- client/src/app/main/products/products.component.spec.ts
+++ client/src/app/main/products/products.component.spec.ts
@@ -1,19 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiService } from '../../shared/api.service';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let apiServiceMock: jasmine.SpyObj<ApiService>;
 
   beforeEach(async () => {
+    apiServiceMock = jasmine.createSpyObj('ApiService', ['getProducts']);
+
     await TestBed.configureTestingModule({
-      imports: [ProductsComponent, HttpClientModule]
-    })
-    .compileComponents();
-    
+      imports: [ProductsComponent, RouterTestingModule],
+      providers: [{ provide: ApiService, useValue: apiServiceMock }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
