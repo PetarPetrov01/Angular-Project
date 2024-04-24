2b45d019d3418c60e365c40cba5d65b058852b8a
PetarPetrov01 <petyr_p@mail.bg>
2024-04-24 22:20:30 +0300

    main component tests setup

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,11 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AuthService } from './shared/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
+import { EMPTY } from 'rxjs';
 
 describe('AppComponent', () => {
+  let authServiceMock: jasmine.SpyObj<AuthService>;
+  let storeMock: jasmine.SpyObj<Store>;
+
   beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', [
+      'getUserStorage',
+      'clearUserSession',
+    ]);
+    storeMock = jasmine.createSpyObj('Store', ['select']);
+
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterTestingModule],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Store, useValue: storeMock },
+      ],
     }).compileComponents();
+
+    storeMock.select.and.returnValue(EMPTY);
+    // authServiceMock.getUserStorage.and.returnValue(EMPTY);
   });
 
   it('should create the app', () => {
@@ -18,12 +38,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('dreamFurniture');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dreamFurniture');
   });
 });
