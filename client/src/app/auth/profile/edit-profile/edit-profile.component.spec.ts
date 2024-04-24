2832f479f8366bf11b4b6b800eee21dccf1334e3
PetarPetrov01 <petyr_p@mail.bg>
2024-04-24 22:11:42 +0300

    Edit-profile, home components test config

--- client/src/app/auth/profile/edit-profile/edit-profile.component.spec.ts
+++ client/src/app/auth/profile/edit-profile/edit-profile.component.spec.ts
@@ -1,18 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditProfileComponent } from './edit-profile.component';
-import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../../../shared/auth.service';
+import { of } from 'rxjs';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('EditProfileComponent', () => {
   let component: EditProfileComponent;
   let fixture: ComponentFixture<EditProfileComponent>;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authServiceMock = jasmine.createSpyObj('AuthService', ['user$']);
     await TestBed.configureTestingModule({
-      imports: [EditProfileComponent, HttpClientModule]
-    })
-    .compileComponents();
-    
+      imports: [
+        EditProfileComponent,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
+    }).compileComponents();
+
+    authServiceMock.user$ = of({
+      _id: '',
+      username: '',
+      email: '',
+      password: '',
+      wishlist: [],
+    });
+
     fixture = TestBed.createComponent(EditProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- client/src/app/main/home/home.component.spec.ts
+++ client/src/app/main/home/home.component.spec.ts
@@ -1,17 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { ApiService } from '../../shared/api.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EMPTY } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let apiServiceMock: jasmine.SpyObj<ApiService>;
 
   beforeEach(async () => {
+    apiServiceMock = jasmine.createSpyObj('ApiService', ['getProducts']);
+
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
-    
+      imports: [HomeComponent, RouterTestingModule],
+      providers: [{ provide: ApiService, useValue: apiServiceMock }],
+    }).compileComponents();
+
+    apiServiceMock.getProducts.and.returnValue(EMPTY);
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
