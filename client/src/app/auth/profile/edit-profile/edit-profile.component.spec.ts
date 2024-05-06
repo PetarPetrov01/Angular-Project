a359b308ba87eedc7e64641a72eb2e9e4dbb0bfa
PetarPetrov01 <petyr_p@mail.bg>
2024-05-06 22:32:36 +0300

    Configure edit profile tests

--- client/src/app/auth/profile/edit-profile/edit-profile.component.spec.ts
+++ client/src/app/auth/profile/edit-profile/edit-profile.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { EditProfileComponent } from './edit-profile.component';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -6,21 +7,35 @@ import { AuthService } from '../../../shared/auth.service';
 import { of } from 'rxjs';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 describe('EditProfileComponent', () => {
   let component: EditProfileComponent;
   let fixture: ComponentFixture<EditProfileComponent>;
   let authServiceMock: jasmine.SpyObj<AuthService>;
+  let fb: jasmine.SpyObj<FormBuilder>;
 
   beforeEach(async () => {
-    authServiceMock = jasmine.createSpyObj('AuthService', ['user$']);
+    authServiceMock = jasmine.createSpyObj('AuthService', [
+      'user$',
+      'editProfile',
+    ]);
+    fb = jasmine.createSpyObj('FormBuilder', ['group']);
     await TestBed.configureTestingModule({
       imports: [
         EditProfileComponent,
         RouterTestingModule,
         BrowserAnimationsModule,
       ],
-      providers: [{ provide: AuthService, useValue: authServiceMock }],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: FormBuilder, useValue: fb },
+      ],
     }).compileComponents();
 
     authServiceMock.user$ = of({
@@ -30,6 +45,13 @@ describe('EditProfileComponent', () => {
       password: '',
       wishlist: [],
     });
+
+    fb.group.and.returnValue(
+      new FormGroup({
+        username: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required),
+      })
+    );
 
     fixture = TestBed.createComponent(EditProfileComponent);
     component = fixture.componentInstance;
