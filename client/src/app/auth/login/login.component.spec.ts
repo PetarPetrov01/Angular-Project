c2b9b2170f703d8ffda0f4e140eaceac31e05cba
PetarPetrov01 <petyr_p@mail.bg>
2024-05-10 20:53:10 +0300

    Setup login component tests

--- client/src/app/auth/login/login.component.spec.ts
+++ client/src/app/auth/login/login.component.spec.ts
@@ -3,19 +3,40 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { AuthService } from '../../shared/auth.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let authServiceMock: jasmine.SpyObj<AuthService>;
+  let fb: jasmine.SpyObj<FormBuilder>;
 
   beforeEach(async () => {
     authServiceMock = jasmine.createSpyObj('AuthService', ['login']);
+    fb = jasmine.createSpyObj('FormBuilder', ['group']);
 
     await TestBed.configureTestingModule({
       imports: [LoginComponent, RouterTestingModule],
-      providers: [{ provide: AuthService, useValue: authServiceMock }],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        {
+          provide: FormBuilder,
+          useValue: fb,
+        },
+      ],
     }).compileComponents();
+
+    fb.group.and.returnValue(
+      new FormGroup({
+        email: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+      })
+    );
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
