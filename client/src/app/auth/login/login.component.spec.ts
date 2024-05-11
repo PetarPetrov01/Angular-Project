9786ee485e2b75b8ab40f9ee87edd8f5288213ac
PetarPetrov01 <petyr_p@mail.bg>
2024-05-11 16:24:33 +0300

    Invalid control/form tests (login component)

--- client/src/app/auth/login/login.component.spec.ts
+++ client/src/app/auth/login/login.component.spec.ts
@@ -76,5 +76,29 @@ describe('LoginComponent', () => {
 
   it('loginForm should be invalid', () => {
     expect(component.loginForm.invalid).toBeTrue();
+    expect(component.loginForm.get('email')?.hasError('required')).toBeTrue();
+    expect(
+      component.loginForm.get('password')?.hasError('required')
+    ).toBeTrue();
+  });
+
+  it('should validate email', () => {
+    component.loginForm.patchValue({
+      email: 'wrong@email',
+    });
+    expect(component.loginForm.get('email')?.hasError('email')).toBeTruthy();
+  });
+
+  it('should not send the request if the inputs are empty', () => {
+    component.handleLoginSubmit();
+    expect(component.isLoading).toBeFalse();
+  });
+
+  it('should not call authService if email is invalid', () => {
+    component.loginForm.patchValue({
+      email: 'invalid@email',
+    });
+
+    expect(authServiceMock.login).not.toHaveBeenCalled();
   });
 });
